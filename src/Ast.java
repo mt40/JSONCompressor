5efdd147b9195c9cbdd0f183e1f26bba33b34fef
minhthai <minhthai_40@gmail.com>
2015-06-01 17:07:24 +0700

    support negative number

--- src/Ast.java
+++ src/Ast.java
@@ -55,7 +55,7 @@ class JsonObject extends Ast {
             pair.compile();
         }
 
-        System.out.println("Pairs = " + pairList.size());
+        //System.out.println("Pairs = " + pairList.size());
     }
 }
 
